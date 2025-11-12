3fe292e15216b064c9a59d31e35f71819d37db22
deris.o2024 <deris.omalley@mymail.champlain.edu>
2025-11-11 22:09:10 -0500

    Start to during launch interface

--- src/components/interface/launch.tsx
+++ src/components/interface/launch.tsx
@@ -1,16 +1,32 @@
 import { useMission } from "../../stores/MissionContext";
+import './styles/launch.css';
+import '../../index.css';
 
 const LaunchInterface = () => {
     const { state } = useMission();
 
-    if (!state.launched && state.fuel < 0) {
-        return null;
-    }
-
     return (
-        <div className="endLaunchInterface_Container">
-            
-        </div>
+        <>
+            { !state.launched && state.fuel > 0 ? (
+                // Probably just velocity and altitude for now
+                <div className="rocket_measurements_container">
+                    {/* Velocity */}
+                    <div className="velocity_tracker_container">
+                        <div className="velocity_tracker">Test</div>
+                    </div>    
+
+                    {/* Altitude */}
+                    <div className="altitude_tracker_container">
+                        <div className="altitude_tracker">Test</div>
+                    </div>
+                </div>
+            ) : (
+                // End Screen Interface
+                <>
+
+                </>
+            )}
+        </>
     )
 }
 
