5a208a7f30fb4c2e73ac19a69fdcd4f1ee0fe2d1
deris.o2024 <deris.omalley@mymail.champlain.edu>
2025-11-11 22:17:57 -0500

    Added timer element and style

--- src/components/interface/launch.tsx
+++ src/components/interface/launch.tsx
@@ -7,19 +7,25 @@ const LaunchInterface = () => {
 
     return (
         <>
-            { !state.launched && state.fuel > 0 ? (
-                // Probably just velocity and altitude for now
-                <div className="rocket_measurements_container">
-                    {/* Velocity */}
-                    <div className="velocity_tracker_container">
-                        <div className="velocity_tracker">Test</div>
-                    </div>    
+            { state.launched && state.fuel > 0 ? (
+                <>
+                    {/* Probably just velocity and altitude for now */}
+                    <div className="rocket_measurements_container">
+                        {/* Velocity */}
+                        <div className="velocity_tracker_container">
+                            <div className="velocity_tracker">{state.velocity}</div>
+                        </div>    
 
-                    {/* Altitude */}
-                    <div className="altitude_tracker_container">
-                        <div className="altitude_tracker">Test</div>
+                        {/* Altitude */}
+                        <div className="altitude_tracker_container">
+                            <div className="altitude_tracker">{state.altitude}</div>
+                        </div>
                     </div>
-                </div>
+
+                    <div className="launch_timer_container">
+                        <p className="missionTime text">{state.missionTime}</p>
+                    </div>
+                </>
             ) : (
                 // End Screen Interface
                 <>
