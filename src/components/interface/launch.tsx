451617a0b9bb637050e5ccfd842b0510494c3f60
deris.o2024 <deris.omalley@mymail.champlain.edu>
2025-11-12 20:55:40 -0500

    Added more style changes to the during launch interface

--- src/components/interface/launch.tsx
+++ src/components/interface/launch.tsx
@@ -7,18 +7,22 @@ const LaunchInterface = () => {
 
     return (
         <>
-            { state.launched && state.fuel > 0 ? (
+            { !state.launched && state.fuel > 0 ? (
                 <>
                     {/* Probably just velocity and altitude for now */}
                     <div className="rocket_measurements_container">
                         {/* Velocity */}
                         <div className="velocity_tracker_container">
-                            <div className="velocity_tracker">{state.velocity}</div>
+                            <span className="velocity_tracker">Velocity</span>
+                            <div className="text_xs">{state.velocity}</div>
+                            <span className="velocity_tracker">KM / H</span>
                         </div>    
 
                         {/* Altitude */}
                         <div className="altitude_tracker_container">
-                            <div className="altitude_tracker">{state.altitude}</div>
+                            <span className="altitude_tracker">Altitude</span>
+                            <span className="text_xs">{state.altitude}</span>
+                            <span className="velocity_tracker">KM</span>
                         </div>
                     </div>
 
