096948d360bc2d6b1ea24d2261355b78b8f8db36
deris.o2024 <deris.omalley@mymail.champlain.edu>
2025-12-02 11:44:14 -0500

    Adjusted orbit controls so user can view around more

--- src/Experience.tsx
+++ src/Experience.tsx
@@ -44,8 +44,8 @@ function App() {
 						<OrbitControls 
 							enableDamping 
 							dampingFactor={0.05}
-							rotateSpeed={0.01}
-							panSpeed={.01}
+							rotateSpeed={0.1}
+							panSpeed={0.1}
 							zoomSpeed={.001}
 							// minDistance={10}
 							// maxDistance={500}
